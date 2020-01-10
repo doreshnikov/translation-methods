9bfb92bd487c444ca5c38d9955cea97a958798b7
amathael <dan.io.oreshnikov@gmail.com>
2020-01-10 04:05:53 +0300

    separated VisitorInfoBuilder and AttributedVisitorInfoBuilder + modified both of them TODO: lateinit primitives? default values? wtf, think something out TODO: actual AttributedVisitorInfoBuilder 	~ think it should generate Visitor: AttributedVisitor<Data, 	Data>

--- include/translate/test/InfoBuildTest.kt
+++ include/translate/test/InfoBuildTest.kt
@@ -1,6 +1,7 @@
 package translate.test
 
 import parse.Parser
+import translate.codegen.AttributedVisitorInfoBuilder
 import translate.codegen.GrammarInfoBuilder
 import translate.codegen.VisitorInfoBuilder
 import translate.meta.grammar.MetaGrammarInfo
@@ -15,16 +16,22 @@ fun main() {
         }
         val uname = singleUpperCase(name)
         try {
+            val root = Parser(MetaGrammarInfo).parse(str)
             val s = GrammarInfoBuilder(
                 "gen.$name",
                 "${uname}GrammarInfo"
-            ).collect(Parser(MetaGrammarInfo).parse(str))
+            ).collect(root)
             File("$loc\\src\\gen\\$name\\${uname}GrammarInfo.kt").bufferedWriter().use { out -> out.write(s) }
             VisitorInfoBuilder(
                 "gen.$name",
                 "${uname}VisitorInfo",
                 "${uname}GrammarInfo"
-            ).doAll(Parser(MetaGrammarInfo).parse(str))
+            ).doAll(root)
+            AttributedVisitorInfoBuilder(
+                "gen.$name",
+                "${uname}AttributedVisitorInfo",
+                "${uname}GrammarInfo"
+            ).doAll(root)
             println("[SUCCESS] $name")
         } catch (e: ParseException) {
             println("[FAIL] $name: $e at ${e.errorOffset}")
